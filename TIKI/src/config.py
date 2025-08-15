571eec92f22cd7098317906e04873eec886a058b
dangtoi05122003 <dangtoi05122003@gmail.com>
2025-08-15 18:15:49 +0700

    add Dockerfile and workflow

--- TIKI/src/config.py
+++ TIKI/src/config.py
@@ -3,4 +3,7 @@ def setup():
     options=Options()
     options.add_argument("--start-maximized")
     options.add_argument("--log-level=3")
+    options.add_argument("headless")
+    options.add_argument("no-sandbox")
+    options.add_argument("disable-dev-shm-usage")
     return options
